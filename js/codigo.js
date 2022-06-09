5239b060dfd1662ebcf11418cc9457229a44ebf4
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-06-09 12:50:13 -0400

    Modificando funcion addToDOM

--- js/codigo.js
+++ js/codigo.js
@@ -2,7 +2,6 @@ let palabraAleatoria = "";
 const time = 10;
 const score = 0;
 
-
 const words = [
     'californication',
     'plataforma5',
@@ -28,22 +27,12 @@ const words = [
 addToDOM(words);
 
 function randomWords(aleatorio) {
-<<<<<<< HEAD
     palabraAleatoria = aleatorio[Math.floor(Math.random() * aleatorio.length)];
     return palabraAleatoria;
 }
 
-function addToDOM(w){
+function addToDOM(word){
     const etiq_h1 = document.querySelector("#randomWord");
-    etiq_h1.textContent = randomWords(w);
+    etiq_h1.textContent = randomWords(word);
 }
 
-=======
-
-    resul = aleatorio[Math.floor(Math.random() * aleatorio.length)];
-
-    return resul;
-}
-
-console.log(randomWords(words))
->>>>>>> f84de5fe4dc3a4b6f790b9dcf22843b3b695f23a
