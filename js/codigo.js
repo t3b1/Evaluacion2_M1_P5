2fcf6f42a8aaaca601e26d63fa3f40ab3e0f9d70
esteban <teba.gonzalez.eg@gmail.com>
2022-06-10 18:40:50 -0400

    funcion del actualizar tiempo

--- js/codigo.js
+++ js/codigo.js
@@ -6,6 +6,9 @@ let score = 0;
 //variable de etiqueta
 const input = document.querySelector("#text")
 
+//variable del tiempo
+let timeInterval = setInterval(actualizarTiempo, 1000);
+
 const words = [
     'californication',
     'plataforma5',
@@ -45,11 +48,25 @@ input.addEventListener("keyup", function(e) {
         if (e.keyCode == 13) {
             if (input.value == palabraAleatoria) {
                 time += 3
+                document.getElementById("text").value = "";
+            }else{
+                document.getElementById("text").value = "";
             }
 
             addToDOM(words)
         }
     }
-
 )
+ function actualizarTiempo(){
+    document.getElementById('timeSpan').innerHTML = time +'s';
 
+        if (time === 0) {
+            clearInterval(timeInterval);
+        
+        }else{
+            time-=1;
+            setTimeout(timeInterval);
+        }
+   
+
+ }
